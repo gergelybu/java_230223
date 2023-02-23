ced4d93e54ad3d10f2bf54475e581e2eb06f90ab
gergelybu <gergelyburai@gmail.com>
2023-02-23 19:25:48 +0100

    Equalizer készítése

--- src/java_0223/Equalizer.java
+++ src/java_0223/Equalizer.java
@@ -1,9 +1,30 @@
-
 package java_0223;
 
+import java.util.Random;
 
 public class Equalizer {
+
+    private static Random rnd = new Random();
+
     public static void main(String[] args) {
-        
+        ciklus();
+
+    }
+
+    private static void ciklus() {
+        int i = 0;
+        while (i < 5) {
+            eq();
+            i += 1;
+        }
+    }
+
+    private static void eq() {
+        int db = rnd.nextInt(3, 8);
+        for (int i = 0; i < db; i++) {
+            System.out.print("\u001B[45m" + " ");
+        }
+        System.out.println("");
+
     }
 }
