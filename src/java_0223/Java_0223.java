c2c229e2ca340e25539bf84a1938c3afa4c86dfc
gergelybu <gergelyburai@gmail.com>
2023-02-23 17:33:53 +0100

    first commit

---
+++ src/java_0223/Java_0223.java
@@ -0,0 +1,30 @@
+package java_0223;
+
+public class Java_0223 {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void elso10SzamOsszege() {
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += i;
+        }
+        return osszeg;
+
+    }
+
+    private static void kiiras(osszeg) {
+        System.out.println("az első 10 szám összege: " + osszeg);
+    }
+    
+    private static void osszead(int a, int b){
+        int c = a + b;
+        return c;
+    }
+    
+    private static void formatzottkiir(int a, int b, int c){
+        System.out.printf("%d + %d = %d\n", a, b, c);
+    }
+}
