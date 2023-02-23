23eec4fbe99473bae10513c2d0fd209c1e763ef4
gergelybu <gergelyburai@gmail.com>
2023-02-23 19:02:35 +0100

    Equalizer

---
+++ src/java_0223/Equalizer.java
@@ -0,0 +1,9 @@
+
+package java_0223;
+
+
+public class Equalizer {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/java_0223/Java_0223.java
+++ src/java_0223/Java_0223.java
@@ -3,7 +3,7 @@ package java_0223;
 public class Java_0223 {
 
     public static void main(String[] args) {
-        kiiras("Az elsõ 10 szám összege: " + elso10SzamOsszege());
+        /*kiiras("Az elsõ 10 szám összege: " + elso10SzamOsszege());
         int szam1 = 1;
         int szam2 = 2;
         int osszeg = osszead(szam1, szam2);
@@ -12,14 +12,19 @@ public class Java_0223 {
         int szam4 = 4;
         osszeg += osszead(szam3, szam4);
         kiiras("%d + %d + %d + %d = %d".formatted(szam1, szam2, szam3, szam4, osszeg));
+        osszeg = osszead(szam2, szam3);
+        osszeg = osszead(osszeg, szam4);
+        double gyok = Math.sqrt(osszeg);
+        kiiras("A gyök érték: " + gyok);*/
+        meghivo();
     }
 
-    private static int elso10SzamOsszege() {
+    private static void elso10SzamOsszege() {
         int osszeg = 0;
         for (int i = 0; i < 10; i++) {
             osszeg += i;
         }
-        return osszeg;
+        kiiras("Az elsõ 10 szám összege: " + osszeg);
 
     }
 
@@ -31,4 +36,38 @@ public class Java_0223 {
     private static int osszead(int a, int b) {
         return a + b;
     }
+
+    private static void meghivo(){
+        elso10SzamOsszege();
+        ketszamosszad();
+        negyosszead();
+        gyokeskiir();
+}
+    private static void negyosszead() {
+        int szam1 = 1;
+        int szam2 = 2;
+        int szam3 = 3;
+        int szam4 = 4;
+        int osszeg = osszead(szam1, szam2);
+        osszeg += osszead(szam3, szam4);
+        kiiras("%d + %d + %d + %d = %d".formatted(szam1, szam2, szam3, szam4, osszeg));
+    }
+    
+     private static void ketszamosszad() {
+        int szam1 = 1;
+        int szam2 = 2;
+        int osszeg = osszead(szam1, szam2);
+        kiiras("%d + %d = %d".formatted(szam1, szam2, osszeg));
+    }
+
+    private static void gyokeskiir() {
+        int szam2 = 2;
+        int szam3 = 3;
+        int szam4 = 4;
+        int osszeg = osszead(szam2, szam3);
+        osszeg = osszead(osszeg, szam4);
+        double gyok = Math.sqrt(osszeg);
+        kiiras("A gyök érték: " + gyok);
+    }
+
 }
