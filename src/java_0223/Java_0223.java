0f216e61dad6613ac7007f9bf3c92ab99be36631
gergelybu <gergelyburai@gmail.com>
2023-02-23 18:41:52 +0100

    négyszám összeadása

--- src/java_0223/Java_0223.java
+++ src/java_0223/Java_0223.java
@@ -3,16 +3,15 @@ package java_0223;
 public class Java_0223 {
 
     public static void main(String[] args) {
-        kiiras("Az els? 10 szám összege: " + elso10SzamOsszege());
+        kiiras("Az elsõ 10 szám összege: " + elso10SzamOsszege());
         int szam1 = 1;
         int szam2 = 2;
         int osszeg = osszead(szam1, szam2);
         kiiras("%d + %d = %d".formatted(szam1, szam2, osszeg));
         int szam3 = 3;
         int szam4 = 4;
-        int osszeg2 = osszead(szam3, szam4);
-        int vegosszeg = osszead(osszeg, osszeg2);
-        kiiras("%d + %d + %d + %d = %d".formatted(szam1, szam2, szam3, szam4, vegosszeg));
+        osszeg += osszead(szam3, szam4);
+        kiiras("%d + %d + %d + %d = %d".formatted(szam1, szam2, szam3, szam4, osszeg));
     }
 
     private static int elso10SzamOsszege() {
